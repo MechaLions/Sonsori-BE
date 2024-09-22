5e6ee73b87e3799396a6b03a6ba3b41a16d5ed1a
sam971114 <simonana@naver.com>
2024-09-23 02:27:40 +0900

    feat: user table column 변경

--- app/models.py
+++ app/models.py
@@ -7,5 +7,5 @@ class User(Base):
     user_id = Column(Integer, primary_key=True, index=True)
     user_login_id = Column(String(255), unique=True, index=True, nullable=False)
     user_login_password = Column(String(255), nullable=False)
-    email = Column(String(255), unique=True, index=True, nullable=False)
-    phone = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False)
+
